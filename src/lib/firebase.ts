94e9a2957b9406a7ad4491139d45c61b243e6468
yash sharma <atrish.yash96@gmail.com>
2025-06-29 00:53:03 +0000

    i'm getting error please pass a valid api key while signin can you chec

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDQ1svXQo3wKtRUWj8BCmcNRBttFkaLOpQ",
+  authDomain: "ai-hackathon-g10x.firebaseapp.com",
+  projectId: "ai-hackathon-g10x",
+  storageBucket: "ai-hackathon-g10x.appspot.com",
+  messagingSenderId: "408362624675",
+  appId: "1:408362624675:web:59a401b8f1d2b80cc4c116",
 };
 
 // Initialize Firebase
